b13db1804dd33d724b4e079059d8eb941ec2b0b7
Van Dyke <daryl_van_dyke@fws.gov>
2016-10-04 13:14:43 -0700

    cleaned up Chi2 template

--- bcm.cpp
+++ bcm.cpp
@@ -1,15 +1,4 @@
 #include "bcm.hpp"
-// double bcm(double m, double b, double *x, double *y, double *yerr, int N) {
-    // int n;
-    // double result = 0.0, diff;
-
-    // for (n = 0; n < N; n++) {
-        // diff = (y[n] - (m * x[n] + b)) / yerr[n];
-        // result += diff * diff;
-    // }
-
-    // return result;
-// }
 int colFromXCoord(double xCoord){
     double xLeft = -374495.83635;
     double cellSize = 270.0;
--- bcm.hpp
+++ bcm.hpp
@@ -1,3 +1,2 @@
 int colFromXCoord(double xCoord);
 int RowFromYCoord(double yCoord);
-//double bcm(double m, double b, double *x, double *y, double *yerr, int N);
